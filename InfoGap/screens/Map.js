0e8355238c340558e0e21aa105e8b8f6911b6ff4
mmunoz122 <150126946+mmunoz122@users.noreply.github.com>
2024-10-21 09:35:21 +0200

    c

--- InfoGap/screens/Map.js
+++ InfoGap/screens/Map.js
@@ -19,7 +19,7 @@ export default function Map({ navigation }) {
           if (id === 1) navigation.navigate("Home"); 
           else if (id === 2) navigation.navigate("Map");
           else if (id === 3) navigation.navigate("Add"); 
-          else if (id === 4) navigation.navigate("Favorite"); 
+          else if (id === 4) navigation.navigate("Favorites"); 
           else if (id === 5) navigation.navigate("Account"); 
         }} 
       />
